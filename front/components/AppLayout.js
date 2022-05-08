a35570741fbd24ac6d9dea6ce1e9679fe14a565a
leyuri <leyuri97@gmail.com>
2022-05-09 06:47:05 +0900

    Add : responsive grid, login form

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,13 +1,22 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Link from 'next/link'
-import { Menu } from 'antd';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+import { Menu } from 'antd'
+import {
+    Input,
+    Col,
+    Row,
+  } from 'antd';
 import 'antd/dist/antd.css'
 
+import UserProfile from './UserProfile'
+import LoginForm from './LoginForm'
+
 const AppLayout = ({ children }) => {
+    const [isLoggedIn] = useState(false)
+
     return (
-        <div>
-            <div>
+            <>
                 <Menu mode='horizontal'>
                     <Menu.Item>
                         <Link href="/"><a>노드버드</a></Link>
@@ -16,12 +25,26 @@ const AppLayout = ({ children }) => {
                         <Link href="/profile"><a>프로필</a></Link>
                     </Menu.Item>
                     <Menu.Item>
+                        <Input.Search enterButton style={{ verticalAlign: 'middle' }}  />
+                    </Menu.Item>
+                    <Menu.Item>
                         <Link href="/signup"><a>회원가입</a></Link>
                     </Menu.Item>
-                </Menu>
-            </div>
-            {children}
-        </div>
+                </Menu> 
+                <Row gutter={8}>
+                <Col xs={24} md={6}>
+                    {isLoggedIn ? <UserProfile /> : <LoginForm/>}
+
+                    </Col>
+                    <Col xs={24} md={12}>
+                    {children}
+
+                    </Col>
+                    <Col xs={24} md={6}>
+                        <a href="https://velog.io/@leyuri" target="_blank" rel="noreferrer noopener">Made by yuri</a>
+                </Col>
+                </Row>
+            </>
     )
 }
 
---
+++ front/components/UserProfile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'antd/dist/antd.css'
+
+const UserProfile = () => {
+
+    return (
+            <>
+                <div>test</div>
+            </>
+    )
+}
+
+export default UserProfile
