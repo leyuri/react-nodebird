86ad61b411fbaa86fd8626484b81d59db2dacfc4
leyuri <leyuri97@gmail.com>
2022-05-26 23:10:31 +0900

    Add : dummy data & PostForm

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import AppLayout from '../components/AppLayout';
+import PostForm from '../components/PostForm';
+import PostCard from '../components/PostCard';
+import { useSelector } from 'react-redux'
 
 const Home = () => {
+
+    const { isLoggedIn } = useSelector((state) => state.user)
+    const { mainPosts }  = useSelector((state) => state.post)
+
     return (
         <AppLayout>
-            <div>
-                Hello, Next!
-            </div>
+            {isLoggedIn && <PostForm />}
+            {mainPosts.map((post) => <PostCard key={post.id} post={post}/>)}
         </AppLayout>
     );
 }
-
+ 
 export default Home
