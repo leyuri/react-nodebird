54f277ffc49662313cec324d8de9ad8fcdafcabb
leyuri <leyuri97@gmail.com>
2022-05-28 09:45:26 +0900

    Add : PostCard

---
+++ front/components/PostImages.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const PostImages = ({ images }) => {
+    return (
+        <div>구현중...</div>
+    )
+};
+
+export default PostImages
